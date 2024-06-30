a9c0e0cbd815aa0c2a0a0de97f0091cc9b4d24a2
Artyom Sirekanyan <58518607+SirArt25@users.noreply.github.com>
2024-06-30 18:13:30 +0400

    [WIP]
    Fixing error for pre-commit.
    
    Just fixing.

--- cmake/apr.cmake
+++ cmake/apr.cmake
@@ -1,58 +1,38 @@
-# First, check the possible locations 
-# of the APR library and find it."
+# First, check the possible locations of the APR library and find it."
 
 set(APR_HEADER_SUPPOSED_PATHS
-    /usr/local/include/apr-1
-    /usr/local/include/apr-1.0
-    /usr/include/apr-1
-    /usr/include/apr-1.0
-    /usr/local/apr/include/apr-1
-)
+    /usr/local/include/apr-1 /usr/local/include/apr-1.0 /usr/include/apr-1
+    /usr/include/apr-1.0 /usr/local/apr/include/apr-1)
 
+find_path(APR_INCLUDE_DIR apr.h PATHS ${APR_HEADER_SUPPOSED_PATHS})
 
-find_path(APR_INCLUDE_DIR apr.h
-        PATHS ${APR_HEADER_SUPPOSED_PATHS}
-)
+set(APR_NAMES apr-1)
 
-set(APR_NAMES
-    apr-1
-)
+set(APR_SUPPOSED_PATHS /usr/lib /usr/local/lib /usr/local/apr/lib)
 
-set(APR_SUPPOSED_PATHS
-    /usr/lib
-    /usr/local/lib
-    /usr/local/apr/lib
-)
+find_library(
+  APR_LIBRARY
+  NAMES ${APR_NAMES}
+  PATHS ${APR_SUPPOSED_PATHS})
 
-find_library(APR_LIBRARY
-        NAMES ${APR_NAMES}
-        PATHS ${APR_SUPPOSED_PATHS}
-)
-
-if (APR_LIBRARY AND APR_INCLUDE_DIR)
-    set(APR_LIBRARIES ${APR_LIBRARY})
-    set(APR_FOUND TRUE)
-else ()
-    set(APR_FOUND FALSE)
-endif ()
-
-
-if (NOT APR_FOUND)
-    MESSAGE(FATAL_ERROR "Could not find APR library")
+if(APR_LIBRARY AND APR_INCLUDE_DIR)
+  set(APR_LIBRARIES ${APR_LIBRARY})
+  set(APR_FOUND TRUE)
+else()
+  set(APR_FOUND FALSE)
 endif()
 
+if(NOT APR_FOUND)
+  message(FATAL_ERROR "Could not find APR library")
+endif()
 
-if (APR_FIND_VERBOSE)
-    MESSAGE(STATUS "Found APR headers: ${APR_INCLUDE_DIR}")
-    MESSAGE(STATUS "Found APR library: ${APR_LIBRARIES}")
-endif ()
-
+if(APR_FIND_VERBOSE)
+  message(STATUS "Found APR headers: ${APR_INCLUDE_DIR}")
+  message(STATUS "Found APR library: ${APR_LIBRARIES}")
+endif()
 
 # Deprecated declarations.
-SET (NATIVE_APR_INCLUDE_PATH ${APR_INCLUDE_DIR} )
-GET_FILENAME_COMPONENT (NATIVE_APR_LIB_PATH ${APR_LIBRARY} PATH)
+set(NATIVE_APR_INCLUDE_PATH ${APR_INCLUDE_DIR})
+get_filename_component(NATIVE_APR_LIB_PATH ${APR_LIBRARY} PATH)
 
-MARK_AS_ADVANCED(
-        APR_LIBRARY
-        APR_INCLUDE_DIR
-)
+mark_as_advanced(APR_LIBRARY APR_INCLUDE_DIR)
