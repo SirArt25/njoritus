649ff12ebd11e68483fe5c4be9f072e0e346763b
Artyom Sirekanyan <58518607+SirArt25@users.noreply.github.com>
2024-07-03 07:32:13 +0400

    [FEAT]
    Add telebot as a external project.
    
    Telebot is C Api for telegram bot.

---
+++ cmake/telebot.cmake
@@ -0,0 +1,17 @@
+include(ExternalProject)
+# Define telebot
+ExternalProject_Add(
+  telebot
+  GIT_REPOSITORY https://github.com/smartnode/telebot.git
+  PREFIX ${CMAKE_BINARY_DIR}/telebot
+  INSTALL_COMMAND "")
+
+ExternalProject_Get_Property(telebot source_dir binary_dir)
+set(TELEBOT_INCLUDE_DIR ${source_dir}/include)
+set(TELEBOT_LIB_DIR ${binary_dir})
+
+include_directories(${TELEBOT_INCLUDE_DIR})
+
+# Create dependecy of njoritus from telebot
+add_dependencies(njoritus telebot)
+target_link_libraries(njoritus ${TELEBOT_LIB_DIR}/libtelebot.so)
