957a10ddc651d451d89c05f8904148b78568a1bd
Artyom Sirekanyan <58518607+SirArt25@users.noreply.github.com>
2024-07-03 13:55:59 +0400

    [WIP]
    Creating external_project folder.

--- cmake/telebot.cmake
+++ cmake/telebot.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 ExternalProject_Add(
   telebot
   GIT_REPOSITORY https://github.com/smartnode/telebot.git
-  PREFIX ${CMAKE_BINARY_DIR}/telebot
+  PREFIX ${EXTERNAL_PROJECTS_DIR}/telebot
   INSTALL_COMMAND "")
 
 ExternalProject_Get_Property(telebot source_dir binary_dir)
@@ -11,6 +11,7 @@ set(TELEBOT_INCLUDE_DIR ${source_dir}/include)
 set(TELEBOT_LIB_DIR ${binary_dir})
 
 include_directories(${TELEBOT_INCLUDE_DIR})
+list(APPEND INCLUDES_LIST ${TELEBOT_INCLUDE_DIR})
 
 # Create dependecy of njoritus from telebot
 add_dependencies(njoritus telebot)
