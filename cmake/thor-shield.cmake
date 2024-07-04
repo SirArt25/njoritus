3fa21c55cc357a11bc3dee4caa80c04b9aab1a2e
Artyom Sirekanyan <58518607+SirArt25@users.noreply.github.com>
2024-07-04 07:40:23 +0400

    [WIP]
    Create thor-shield-lib for njoritus
    
    We will link thor-shield-lib into njoritus

--- cmake/thor-shield.cmake
+++ cmake/thor-shield.cmake
@@ -8,7 +8,12 @@ set(THOR_SHIELD_INLUDE_LIST ${INCLUDES_LIST}/thor-shield)
 
 add_executable(thor-shield src/thor-shield/main.c src/thor-shield/enc.c
                            src/thor-shield/dec.c src/thor-shield/mjolnir.c)
-target_include_directories(thor-shield PUBLIC ${THOR_SHIELD_INLUDE_LIST})
+target_include_directories(thor-shield PRIVATE ${THOR_SHIELD_INLUDE_LIST})
 
 target_link_libraries(thor-shield ${LIBSECRET_LIBRARIES})
 add_definitions(${LIBSECRET_CFLAGS_OTHER})
+
+add_library(thor-shield-lib src/thor-shield/dec.c src/thor-shield/mjolnir.c
+                            src/thor-shield/enc.c)
+
+target_include_directories(thor-shield-lib PRIVATE ${THOR_SHIELD_INLUDE_LIST})
