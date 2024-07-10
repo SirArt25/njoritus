de687cca81d485f66e7ccf99a3131e149f17298c
Artyom Sirekanyan <58518607+SirArt25@users.noreply.github.com>
2024-07-10 19:55:39 +0400

    [WIP]
    Create tests.yml and Update build.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source ${SCRIPT_DIR}/utilities.sh
 
 usage() {
-  echo "Usage: $0 {build|rebuild|configure|build_with_checks}"
+  echo "Usage: $0 {build|rebuild|configure|build_with_checks|"
+  echo "             configure_with_test|build_with_test|"
+  echo "             rebuild_with_test|"
+  echo "             build_with_checks_and_tests|}"
   exit 1
 }
 
@@ -15,17 +18,30 @@ fi
 
 case $1 in
   build)
-    build
+    build false
     ;;
   build_with_checks)
-    build_with_checks
+    build_with_checks false
+    ;;
+  build_with_test)
+    build true
+    ;;
+  build_with_checks_and_tests)
+    build_with_checks true
     ;;
   configure)
-    configure
+    configure false
+    ;;
+  configure_with_test)
+    configure true
     ;;
   rebuild)
     clean
-    build
+    build false
+    ;;
+  rebuild_with_test)
+    clean
+    build true
     ;;
   *)
     usage
